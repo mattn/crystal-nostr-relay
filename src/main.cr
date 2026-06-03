8d237b418831bcca2addeeea49d2760a011fde68
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-06-03 20:59:13 +0900

    NIP-11: add relay_countries (configurable via RELAY_COUNTRIES, default JP)

--- src/main.cr
+++ src/main.cr
@@ -12,6 +12,7 @@ RELAY_INFO = {
   contact: ENV["RELAY_CONTACT"]? || "",
   icon: ENV["RELAY_ICON"]? || "",
   supported_nips: [1, 2, 4, 9, 11, 12, 15, 16, 20, 28, 33, 40, 45, 65, 70],
+  relay_countries: (ENV["RELAY_COUNTRIES"]? || "JP").split(',').map(&.strip).reject(&.empty?),
   software: "https://github.com/mattn/crystal-nostr-relay",
   version: "0.1.0",
 }
